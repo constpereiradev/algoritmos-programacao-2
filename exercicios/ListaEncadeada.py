28cad90889b6a796c13b5c78d7676d87b4294075
Amanda Pereira <107977902+constpereiradev@users.noreply.github.com>
2025-04-02 19:45:43 -0300

    Update ListaEncadeada.py

--- exercicios/ListaEncadeada.py
+++ exercicios/ListaEncadeada.py
@@ -8,6 +8,10 @@ class Nota:
     self.nota1 = nota1
     self.nota2 = nota2
 
+  def alteraNome(self, nome):
+    self.nome = nome
 
-n = Nota("SeuNomeAqui", 10, 10)
+
+n = Nota("Amanda", 10, 10)
+n.alteraNome("Amanda Pereira")
 print(n.nome, n.nota1, n.nota2)
